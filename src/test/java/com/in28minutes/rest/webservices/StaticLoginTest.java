f471cabcfd46f244eb6e8fd5540978c347396a7c
Wei Jin Kok <wkok3@gatech.edu>
2023-06-14 19:00:46 -0400

    StaticLoginTest now able to load spring application context and test Spring boot application see: https://qaautomation.expert/2023/03/09/testing-of-springboot-application-with-testng/

--- src/test/java/com/in28minutes/rest/webservices/StaticLoginTest.java
+++ src/test/java/com/in28minutes/rest/webservices/StaticLoginTest.java
@@ -2,45 +2,67 @@ package com.in28minutes.rest.webservices;
 
 import static org.testng.Assert.assertEquals;
 
+import org.json.JSONException;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.in28minutes.rest.webservices.restfulwebservices.RestfulWebServicesApplication;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class StaticLoginTest extends AbstractChromeWebDriverTest {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = RestfulWebServicesApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StaticLoginTest extends  AbstractTestNGSpringContextTests {
 
-//	private WebDriver driver;
-//
-//	@BeforeTest
-//	public void beforeTest() {
-//		WebDriverManager.chromedriver().setup();
-//
-//		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--remote-allow-origins=*");
-//		 driver = new ChromeDriver(options);
-//	}
+	@LocalServerPort
+	private int port;
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	HttpHeaders headers = new HttpHeaders();
 
 	@Test
 	public void f() {
-		//
 
-//		WebDriverManager.chromedriver().setup();
-//
-//		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--remote-allow-origins=*");
-//		WebDriver driver = new ChromeDriver(options);
-		driver.get("http://www.google.com");
-//		System.out.println(driver.getTitle());
-		assertEquals("Google", driver.getTitle());
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/user_follow/get/all"),
+				HttpMethod.GET, entity, String.class);
+
+		// For what to put as expected response to test, send request at POSTMAN and
+		// look at response values
+		String expected = "{\"status\":\"OK\",\"message\":\"No users following any comments\",\"code\":200}";
+
+		try {
+			JSONAssert.assertEquals(expected, response.getBody(), false);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		sleep(10);
+//		driver.get("http://localhost:4200");
+////		System.out.println(driver.getTitle());
+//		assertEquals("Mooc app", driver.getTitle());
 
 	}
-	
-//	@AfterTest
-//	public void afterTest() {
-//		driver.quit();
-//	}
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
+	}
 }
